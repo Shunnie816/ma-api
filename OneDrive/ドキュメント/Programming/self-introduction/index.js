750b763bceb51e7a50155058d94c1105482c6ae7
ShunYoshiya <ponyo.shun0816@gmail.com>
2022-04-26 13:59:28 +0900

    add modal function

--- OneDrive/ドキュメント/Programming/self-introduction/index.js
+++ OneDrive/ドキュメント/Programming/self-introduction/index.js
@@ -1,3 +1,4 @@
+// アコーディオンメニュー 
 const titles = document.getElementsByClassName('acd');
 function accordion() {
     const acd = this.nextElementSibling;
@@ -8,3 +9,25 @@ function accordion() {
 for (i = 0; i < titles.length; i++) {
     titles[i].addEventListener("click", accordion);
 }
+
+// モーダルウィンドウ
+const messageBtn = document.getElementById('btn');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close-btn');
+
+messageBtn.addEventListener("click", modalOpen);
+function modalOpen() {
+    modal.style.display = 'block';
+};
+
+closeBtn.addEventListener("click", modalClose);
+function modalClose() {
+    modal.style.display = 'none';
+};
+
+addEventListener('click', outsideClose);
+function outsideClose(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    };
+};
