28d4d56976bb91c6ef833083ded85580580a6d25
ShunYoshiya <ponyo.shun0816@gmail.com>
2022-08-12 10:27:27 +0900

    apply swith image

--- Programming/Shunnie's room/script.js
+++ Programming/Shunnie's room/script.js
@@ -1,47 +1,8 @@
-// アコーディオンメニュー 
-const titles = document.getElementsByClassName('acd');
-function accordion() {
-    const acd = this.nextElementSibling;
-    acd.classList.toggle('closed');
-    acd.classList.toggle('is-open');
-}
+'use strict'
 
-for (i = 0; i < titles.length; i++) {
-    titles[i].addEventListener("click", accordion);
-}
-
-// モーダルウィンドウ
-const messageBtn = document.getElementById('btn');
-const modal = document.getElementById('modal');
-const closeBtn = document.getElementById('close-btn');
-
-messageBtn.addEventListener("click", modalOpen);
-function modalOpen() {
-    modal.style.display = 'block';
-};
-
-closeBtn.addEventListener("click", modalClose);
-function modalClose() {
-    modal.style.display = 'none';
-};
-
-addEventListener('click', outsideClose);
-function outsideClose(e) {
-    if (e.target == modal) {
-        modal.style.display = 'none';
-    };
-};
-
-// Swiper.jsのサンプルコード
-const swiper = new Swiper(".swiper-container", {
-    // ドットインジケーターの表示
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    // 前後スライドボタンを表示
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    loop: true, // ループの有効化
+const thumbs = document.querySelectorAll('.thumb');
+thumbs.forEach(function (item, index) {
+    item.onclick = function () {
+        document.getElementById('bigimg').src = this.dataset.image;
+    }
 });
