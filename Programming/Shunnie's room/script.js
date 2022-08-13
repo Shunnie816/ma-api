1b4d06a9617f9b93d53bccdc6279393a9e96adea
ShunYoshiya <ponyo.shun0816@gmail.com>
2022-08-13 17:34:24 +0900

    add number for slideshow

--- Programming/Shunnie's room/script.js
+++ Programming/Shunnie's room/script.js
@@ -19,6 +19,22 @@ const images = [
     'https://github.com/Shunnie816/Shunnies-room/blob/master/sunset.jpg?raw=true'
 ];
 
+const extraImages = [
+    'https://github.com/Shunnie816/Shunnies-room/blob/master/my_photo.jpg?raw=true',
+    'https://github.com/Shunnie816/Shunnies-room/blob/master/piano.jpg?raw=true',
+    'https://github.com/Shunnie816/Shunnies-room/blob/master/guitar.jpg?raw=true',
+    'https://github.com/Shunnie816/Shunnies-room/blob/master/bike.jpg?raw=true',
+    'https://github.com/Shunnie816/Shunnies-room/blob/master/game.jpg?raw=true'
+];
+
+images.forEach(function (item, index) {
+    preloadImage(item);
+});
+
+extraImages.forEach(function (item, index) {
+    preloadImage(item);
+}); 
+
 let current = 0;
 const mainImage = document.getElementById('main-image');
 
@@ -26,8 +42,15 @@ function changeImage(num) {
     if (current + num >= 0 && current + num < images.length) {
         current += num;
         mainImage.style.backgroundImage = `url(${images[current]})`;
+        pageNum();
     }
 };
+
+function pageNum() {
+    document.getElementById('page').textContent = `${current + 1} / ${images.length}`;
+}
+
+pageNum();
 
 document.getElementById('prev').onclick = function () {
     changeImage(-1);
@@ -38,3 +61,9 @@ document.getElementById('next').onclick = function () {
     changeImage(1);
     console.log("右がクリックされました");
 };
+
+// 画像をあらかじめ読み込む
+function preloadImage(path) {
+    let imgTag = document.createElement('img');
+    imgTag.src = path;
+}
